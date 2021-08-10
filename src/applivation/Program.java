ad31329166498dd536778d6fdd513d2e8f0c2511
AmonCardim <amoncardim@gmail.com>
2021-08-10 17:25:52 -0300

    First class: Position

--- src/applivation/Program.java
+++ src/applivation/Program.java
@@ -1,9 +1,19 @@
 package applivation;
 
+import java.util.Scanner;
+
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world!S");
+		Scanner sc = new Scanner(System.in);
+		
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
+		
+		
+		sc.close();
 	}
 
 }

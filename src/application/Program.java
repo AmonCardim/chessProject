096ae6d1cac340174e130ef1c661450ed544ffe6
AmonCardim <amoncardim@gmail.com>
2021-08-11 12:23:12 -0300

    Placing pieces on the board

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,19 @@
 package application;
 
+import java.util.Scanner;
+
 import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
-		 
+		Scanner sc = new Scanner(System.in);
+		
 		ChessMatch chessMatch = new ChessMatch();
 		UI.printBoard(chessMatch.getPieces());
 		
 		
-		
+		sc.close();
 	}
 
 }
